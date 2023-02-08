ff2694cdc1d16899610f4d23a6f5f12e73ba00d3
HEHEHAHAWHAT <122508772+HEHEHAHAWHAT@users.noreply.github.com>
2023-02-08 13:20:26 -0500

    Add files via upload

---
+++ Itr1/BookSearch.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BookSearch extends bookOrganization {
+  
+    public BookSearch() {
+        super();
+    }
+  
+    public BookSearch(ArrayList<Book> bookShelf) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the String for search (author, id or bookName): ");
+        String searchResult = s.nextLine();
+        boolean bookFound = false;
+        for (int i = 0; i < bookShelf.size(); i++) 
+        {
+            Book book = bookShelf.get(i);
+            if (book.getAuthor().equalsIgnoreCase(searchResult)||book.getBookId().equalsIgnoreCase(searchResult)||book.getBookName().equalsIgnoreCase(searchResult))
+            {
+                System.out.println("Book Name: " + book.getBookName());
+                System.out.println("Author: " + book.getAuthor());
+                System.out.println("Book ID: " + book.getBookId());
+                System.out.println("Category: " + book.getCategory());
+                System.out.println("Price: " + book.getPrice());
+                bookFound = true;
+                break;
+            } 
+            
+        
+    }
+        if (!bookFound) {
+            System.out.println("Book not found!");
+        }
+    }
+}
