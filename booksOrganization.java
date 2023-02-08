1ec92172df57578ca4043ad32723562796d08c5f
yunfeiCao <77178977+yunfeiCao@users.noreply.github.com>
2023-02-07 21:53:04 -0500

    I add the deleteBooks function

--- booksOrganization.java
+++ booksOrganization.java
@@ -14,9 +14,23 @@ public class booksOrganization {
         System.out.println("Book added successfully!");
     }
  
-public void deleteBooks(){
-
-}   
+public void deleteBooks(String bookName, String bookId) {
+	    int i = 0;
+	    boolean deleted = false;
+	     while (i < bookShelf.size()) {
+	      Book book = bookShelf.get(i);
+	        if (bookName.equals(book.getBookName()) && bookId.equals(book.getBookId())) {
+	         bookShelf.remove(book);
+	          System.out.println("Book deleted successfully!");
+	            deleted = true;
+	             break;
+	          }
+	          i++;
+	        }
+	        if (!deleted) {
+	            System.out.println("Please check the book name and id, try again!");
+	        }
+	    }  
 
 public void removeAllbooks() {
 	bookShelf.clear();
