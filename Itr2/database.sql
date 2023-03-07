1213f59a46595b153886d8a4af8562237eea6f6e
HEHEHAHAWHAT <nzskfree@my.yorku.ca>
2023-03-07 12:26:11 -0500

    Create database.sql

---
+++ Itr2/database.sql
@@ -0,0 +1,17 @@
+create database library;
+use library;
+create table Book(
+ timeID int unsigned auto_increment primary key,
+ bookID varchar(100) not null,
+ bookName varchar (1000) not null,
+ author varchar (1000) not null,
+ category varchar (1000) not null,
+ price varchar(1000) not null
+);
+
+create table Bookingroom(
+id int unsigned auto_increment primary key,
+roomNumber int(10) not null,
+BookingId int (9) not null,
+BookingName varchar(1000) not null
+);
