4cedbf3eb98da4c762036563a7b145e593fe48cb
HEHEHAHAWHAT <nzskfree@my.yorku.ca>
2023-03-07 14:54:42 -0500

    Update database.sql

--- Itr2/database.sql
+++ Itr2/database.sql
@@ -6,12 +6,15 @@ create table Book(
  bookName varchar (1000) not null,
  author varchar (1000) not null,
  category varchar (1000) not null,
- price varchar(1000) not null
+ price varchar(1000) not null,
+ borrowTime datetime not null,
+ returnTime datetime default null
 );
 
 create table Bookingroom(
 id int unsigned auto_increment primary key,
 roomNumber int(10) not null,
 BookingId int (9) not null,
-BookingName varchar(1000) not null
+BookingName varchar(1000) not null,
+Bookingtime timestamp default current_timestamp on update current_timestamp
 );
