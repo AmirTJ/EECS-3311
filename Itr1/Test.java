7f4004707adbd4b8795b4b33c17d2dd8e26a18bb
Winfred <91762181+Winfred01@users.noreply.github.com>
2023-03-08 01:42:04 -0500

    linked whole booksOrganization class to MySQL
    update all the method link to MySQL, and they all pass the simple test code.

--- Itr1/Test.java
+++ Itr1/Test.java
@@ -1,10 +1,15 @@
 public class Test {
     public static void main (String []args){
         BookSearch bb= new BookSearch();
-        bb.addBooks("hh", "hh2", "hhh", "goodlarry", 2);
-        bb.addBooks("abcab", "aaaa", "bbbb", "cccc", 0);
-        bb.addBooks("abcab", "aaab", "bbbb", "cccc", 0);
-        bb.addBooks("abcab", "aaaa", "bbcb", "cccc", 0);
+        bb.removeAllbooks();  // clear the database
+
+        bb.addBooks("hh", "hh2", "hhh", "goodlarry", 2); // Book added successfully
+        bb.addBooks("abcab", "aaaa", "bbbb", "cccc", 0); // Book added successfully
+        bb.addBooks("abcab", "aaab", "bbbb", "cccc", 0); // Book added successfully
+        bb.addBooks("abcab", "aaaa", "bbcb", "cccc", 0); // return error, already have same book ID
+
+        bb.deleteBooks("abcab", "aaaa"); // should delete successfully
+        bb.deleteBooks("aaaa", "abcab"); // return error, there is no book to delete
 
         bb.searchById("hh2");
 	}
--- Itr1/booksOrganization.java
+++ Itr1/booksOrganization.java
@@ -15,7 +15,16 @@ public class booksOrganization {
 	}
 
 	public void addBooks(String bookName, String bookId, String author, String category, double price) {
+		// bookShelf check book ID
+		for(int i=0;i<bookShelf.size();i++) {
+			Book check = bookShelf.get(i);
+			if(bookId.equals(check.getBookId())) {
+				System.out.print("In bookShelf: There has a same book id in the list, please check again!\n");
+			}
 
+		}
+
+		// database check book ID
 		String query = "SELECT COUNT(*) FROM library.book WHERE bookID=?";
 		try (Connection con = DriverManager.getConnection(url, user, password);
 			 PreparedStatement statement = con.prepareStatement(query)) {
@@ -29,14 +38,7 @@ public class booksOrganization {
 			e.printStackTrace();
 		}
 
-		for(int i=0;i<bookShelf.size();i++) {
-			Book check = bookShelf.get(i);
-			if(bookId.equals(check.getBookId())) {
-				System.out.print("In bookShelf: There has a same book id in the list, please check again!\n");
-				return;
-			}
 
-		}
 		Book book = new Book(bookName, bookId, author, category, price);
 		book.setBookName(bookName);
 		book.setBookId(bookId);
@@ -65,26 +67,54 @@ public class booksOrganization {
 	}
 
 	public void deleteBooks(String bookName, String bookId) {
+		// bookShelf
 		int i = 0;
 		boolean deleted = false;
 		while (i < bookShelf.size()) {
 			Book book = bookShelf.get(i);
 			if (bookName.equals(book.getBookName()) && bookId.equals(book.getBookId())) {
 				bookShelf.remove(book);
-				System.out.println("Book deleted successfully!");
+				System.out.println("In bookShelf: Book deleted successfully!");
 				deleted = true;
 				break;
 			}
 			i++;
 		}
 		if (!deleted) {
-			System.out.println("Please check the book name and id, try again!");
+			System.out.println("In bookShelf: Please check the book name and id, try again!");
+		}
+
+		// database
+		String query = "DELETE FROM library.book WHERE bookID=? AND bookName=?";
+		try (Connection con = DriverManager.getConnection(url, user, password);
+			 PreparedStatement statement = con.prepareStatement(query)) {
+			statement.setString(1, bookId);
+			statement.setString(2, bookName);
+			int rowsAffected = statement.executeUpdate();
+			if (rowsAffected == 0) {
+				System.out.println("In database: Please check the book name and id, try again!");
+			} else {
+				System.out.println("In database: Book deleted successfully!");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 
 	public void removeAllbooks() {
+		// bookShelf
 		bookShelf.clear();
-		System.out.println("All books had been removed!");
+		System.out.println("In bookShelf: All books had been removed!");
+
+		// database
+		String query = "DELETE FROM library.book";
+		try (Connection con = DriverManager.getConnection(url, user, password);
+			 PreparedStatement statement = con.prepareStatement(query)) {
+			int rowsAffected = statement.executeUpdate();
+			System.out.println(rowsAffected + " books had been removed in database!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 
