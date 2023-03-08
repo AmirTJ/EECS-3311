e9c9bb28810281b60de443036170247cb86d192d
qisen zhong <122420908+sen222ffds@users.noreply.github.com>
2023-03-08 18:31:11 -0500

    add few  case and removeall book before each case

--- Itr2/JUnitTest.java
+++ Itr2/JUnitTest.java
@@ -12,6 +12,7 @@ public class JUnitTest {
 	@Before
 	public void setUp() throws Exception {
 		bookOrg = new booksOrganization();
+		bookOrg.removeAllbooks();
 	}
 	
 	@Test
@@ -25,6 +26,33 @@ public class JUnitTest {
 		assertEquals("History", books.get(0).getCategory());
 		assertEquals(10.99, books.get(0).getPrice(), 0.001);
 	}
+	
+	@Test
+	public void testAddThreeBooks() {
+		bookOrg.addBooks("ShangHai history", "002", "Tony", "History", 7.99);
+		bookOrg.addBooks("GuangZhou history", "003", "Tony", "History", 9.99);
+		bookOrg.addBooks("BeiJing history", "004", "Tony", "History", 8.99);
+		ArrayList<Book> books = bookOrg.getBooks();
+		assertEquals(3, books.size());
+		assertEquals("004", books.get(2).getBookId());
+		assertEquals("BeiJing history", books.get(2).getBookName());
+		assertEquals("Tony", books.get(2).getAuthor());
+		assertEquals("History", books.get(2).getCategory());
+		assertEquals(8.99, books.get(2).getPrice(), 0.001);
+	}
+	
+	@Test
+	public void testAddBookEmptyName() {
+		bookOrg.addBooks("", "", "", "", 0);
+		ArrayList<Book> books = bookOrg.getBooks();
+		assertEquals(1, books.size());
+		assertEquals("", books.get(0).getBookId());
+		assertEquals("", books.get(0).getBookName());
+		assertEquals("", books.get(0).getAuthor());
+		assertEquals("", books.get(0).getCategory());
+		assertEquals(0, books.get(0).getPrice(), 0.001);
+	
+	}
 
 	@Test
 	public void testDeleteBooks() {
@@ -32,6 +60,23 @@ public class JUnitTest {
 		bookOrg.deleteBooks("Chinese history", "001");
 		ArrayList<Book> books = bookOrg.getBooks();
 		assertEquals(0, books.size());
+	}
+	
+	@Test
+	public void testDeletdSecondBookInThreeBooks() {
+		bookOrg.addBooks("ShangHai history", "002", "Tony", "History", 7.99);
+		bookOrg.addBooks("GuangZhou history", "003", "Tonyz", "Math", 9.99);
+		bookOrg.addBooks("BeiJing history", "004", "Tony", "History", 8.99);
+		bookOrg.deleteBooks("GuangZhou history", "003");
+		ArrayList<Book> books = bookOrg.getBooks();
+		assertEquals(2, books.size());
+		assertNotEquals("GuangZhou",books.get(1).getBookName());
+		assertNotEquals("003",books.get(1).getBookId());
+		assertNotEquals("Tonyz",books.get(1).getAuthor());
+		assertNotEquals("Math",books.get(1).getCategory());
+		assertNotEquals(9.99,books.get(1).getPrice(), 0.001);
+		assertEquals("BeiJing history",books.get(1).getBookName());
+		assertEquals("004",books.get(1).getBookId());
 	}
 
 	@Test
