a4bcedefc151bef57cac9122cc5fc321e3b9c912
HEHEHAHAWHAT <nzskfree@my.yorku.ca>
2023-03-08 17:25:12 -0500

    Create StudentCount.java

---
+++ Itr2/StudentCount.java
@@ -0,0 +1,29 @@
+package Itr2;
+import java.sql.*;
+
+public class StudentCount {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "";
+            String user = ""; // 
+            String password = ""; // 
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement st = connection.createStatement();
+            String sql = "SELECT username, password FROM users WHERE role = 'student'";
+            ResultSet rs = st.executeQuery(sql);
+          while (rs.next()) {
+            String username = rs.getString("username");
+            String password1 = rs.getString("password");
+            System.out.println("Username: " + username + ", Password: " + password1);
+          }
+          rs.close();
+          st.close();
+          connection.close();
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+}
