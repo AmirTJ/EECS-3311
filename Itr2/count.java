4047d7d10530bfe18dcba572f8adb28393a0e98f
HEHEHAHAWHAT <nzskfree@my.yorku.ca>
2023-03-08 15:39:07 -0500

    Create count.java

---
+++ Itr2/count.java
@@ -0,0 +1,12 @@
+package Itr2;
+import java.sql.*;
+
+
+public class count extends User{
+
+    public count(String username, String password, String role) {
+        super(username, password, role);
+       
+    }
+    
+}
