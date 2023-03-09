5c4ca3b9e26bbfc6bf5650cbc3311830893d60a8
AmirTJ <77169817+AmirTJ@users.noreply.github.com>
2023-03-08 19:41:01 -0500

    Add files via upload
    New User Story Borrowing and Extension of the duration by our clients.

---
+++ Itr2/Borrow.java
@@ -0,0 +1,66 @@
+package Itr2;
+
+import java.util.Date;
+public class Borrow {
+ 
+     private String title;
+     private String author;
+     private boolean borrowed;
+     private Date borrowedDate;
+     private Date dueDate;
+     
+     public Borrow(String title, String author) {
+         this.title = title;
+         this.author = author;
+         this.borrowed = false;
+         this.borrowedDate = null;
+         this.dueDate = null;
+     }
+     
+     public String getTitle() {
+         return title;
+     }
+     
+     public String getAuthor() {
+         return author;
+     }
+     
+     public boolean isBorrowed() {
+         return borrowed;
+     }
+     
+     public Date getBorrowedDate() {
+         return borrowedDate;
+     }
+     
+     public Date getDueDate() {
+         return dueDate;
+     }
+     
+     public void borrowBook() {
+         if (borrowed) {
+             System.out.println("Sorry, this book is already borrowed.");
+         } else {
+             borrowed = true;
+             borrowedDate = new Date();
+             dueDate = new Date(borrowedDate.getTime() + (7 * 24 * 60 * 60 * 1000)); // 7 days from borrowedDate
+             System.out.println("Book borrowed successfully. Please return it by " + dueDate);
+         }
+     }
+     
+     public void extendDueDate(int days) {
+         if (borrowed) {
+             dueDate = new Date(dueDate.getTime() + (days * 24 * 60 * 60 * 1000));
+             System.out.println("Due date extended successfully. Please return the book by " + dueDate);
+         } else {
+             System.out.println("Sorry, this book is not borrowed yet.");
+         }
+     }
+     
+     public void returnBook() {
+         borrowed = false;
+         borrowedDate = null;
+         dueDate = null;
+         System.out.println("Book returned successfully.");
+     }
+ }
