0616b3ad35e537c212484703c74ae8ad27bdad4a
HEHEHAHAWHAT <nzskfree@my.yorku.ca>
2023-03-09 16:06:51 -0500

    Update database.sql

--- Itr2/database.sql
+++ Itr2/database.sql
@@ -7,7 +7,7 @@ create table Book(
  author varchar (1000) not null,
  category varchar (1000) not null,
  price varchar(1000) not null,
- lend int not null,
+ lend int DEFAULT 0,
  borrowTime datetime not null,
  returnTime datetime default null
 );
