08223604f74d91e5cf70dd98c91ea816af68e7ae
qisen zhong <122420908+sen222ffds@users.noreply.github.com>
2023-03-09 22:18:21 -0500

    Add few cases and fixed bug

--- src/Itr2/Itr2/JUnitTest.java
+++ src/Itr2/Itr2/JUnitTest.java
@@ -1,24 +1,28 @@
 package Itr2.Itr2;
 import Itr1.*;
+
 import static org.junit.Assert.*;
-
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class JUnitTest {
+	
 
 	private booksOrganization bookOrg;
+	private BookSearch bookSea;
 	
 	@Before
 	public void setUp() throws Exception {
 		bookOrg = new booksOrganization();
+		bookSea = new BookSearch();
 		bookOrg.removeAllbooks();
 	}
 	
 	@Test
-	public void testAddBooks() {
+	public void testAddBooksBaseCase() {
 		bookOrg.addBooks("Chinese history", "001", "Yunfei", "History", 10.99);
 		ArrayList<Book> books = bookOrg.getBooks();
 		assertEquals(1, books.size());
@@ -27,8 +31,9 @@ public class JUnitTest {
 		assertEquals("Yunfei", books.get(0).getAuthor());
 		assertEquals("History", books.get(0).getCategory());
 		assertEquals(10.99, books.get(0).getPrice(), 0.001);
-	}
 	
+	}
+
 	@Test
 	public void testAddThreeBooks() {
 		bookOrg.addBooks("ShangHai history", "002", "Tony", "History", 7.99);
@@ -41,9 +46,7 @@ public class JUnitTest {
 		assertEquals("Tony", books.get(2).getAuthor());
 		assertEquals("History", books.get(2).getCategory());
 		assertEquals(8.99, books.get(2).getPrice(), 0.001);
-		
 	}
-	
 	@Test
 	public void testAddBookEmptyName() {
 		bookOrg.addBooks("", "", "", "", 0);
@@ -56,15 +59,16 @@ public class JUnitTest {
 		assertEquals(0, books.get(0).getPrice(), 0.001);
 	
 	}
-
+	
+	
 	@Test
-	public void testDeleteBooks() {
+	public void testDeleteBooksBasecase() {
 		bookOrg.addBooks("Chinese history", "001", "Yunfei", "History", 10.99);
 		bookOrg.deleteBooks("Chinese history", "001");
 		ArrayList<Book> books = bookOrg.getBooks();
 		assertEquals(0, books.size());
 	}
-	
+
 	@Test
 	public void testDeletdSecondBookInThreeBooks() {
 		bookOrg.addBooks("ShangHai history", "002", "Tony", "History", 7.99);
@@ -81,14 +85,55 @@ public class JUnitTest {
 		assertEquals("BeiJing history",books.get(1).getBookName());
 		assertEquals("004",books.get(1).getBookId());
 	}
-
+	
 	@Test
-	public void testRemoveAllBooks() {
+	public void testSearchBookById() {
+		Book a = new Book("001", "C", "Y", "H", 10.99);
+	bookSea.addBooks("C", "001", "Y", "H", 10.99);
+		Book b =bookSea.searchByID("001");
+		 assertNotNull(b);
+	        assertEquals(a.getBookId(), b.getBookId());
+	}
+	
+	@Test
+	public void testSearchBookByBookName() {
+		Book a = new Book("001", "Chinese History", "Tony", "History", 9.99);
+	bookSea.addBooks("Chinese History", "001", "Tony", "History", 9.99);
+		Book b =bookSea.searchByName("Chinese History");
+		 assertNotNull(b);
+	        assertEquals(a.getBookName(), b.getBookName());
+	}
+	
+	@Test
+	public void testSearchBookByAuthor() {
+		Book a = new Book("Chinese History", "001", "History", "Tony", 9.99);
+	bookSea.addBooks("Chinese History", "001", "Tony", "History", 9.99);
+	bookSea.addBooks("GuangZhou History", "002", "Lux", "History", 8.99);
+		Book b =bookSea.searchByAuthor("Tony");
+		 assertNotNull(b);
+	        assertEquals(a.getAuthor(), b.getCategory());
+	        assertEquals(a.getBookId(), b.getBookId());
+	}
+	
+	@Test
+	public void testSearchBookByCategory() {
+		Book a = new Book("GuangZhou History", "002", "Jack", "arts", 8.99);
+	bookSea.addBooks("Chinese History", "001", "Tony", "History", 9.99);
+	bookSea.addBooks("GuangZhou History", "002", "Jack", "arts", 8.99);
+		Book b =bookSea.searchByCategory("arts");
+		 assertNotNull(b);
+	        assertEquals(a.getCategory(), b.getCategory());
+	        assertEquals(a.getBookId(), b.getBookId());
+	}
+	
+	@Test
+	public void testRemoveAllBooksBasecase() {
 		bookOrg.addBooks("Chinese history", "001", "Yunfei", "History", 10.99);
 		bookOrg.addBooks("FIFA word cup 2018", "002", "Yunxiang", "Sport", 9.99);
 		bookOrg.removeAllbooks();
 		ArrayList<Book> books = bookOrg.getBooks();
 		assertEquals(0, books.size());
 	}
+	
+	
 }
-
