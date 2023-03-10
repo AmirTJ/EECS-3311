0f62757b745c91419cababf5d7876ae33ae90286
qisen zhong <122420908+sen222ffds@users.noreply.github.com>
2023-03-09 22:19:14 -0500

    add few cases and fixed bug

--- src/Itr2/Itr2/IntegrationTest.java
+++ src/Itr2/Itr2/IntegrationTest.java
@@ -1,7 +1,10 @@
 package Itr2.Itr2;
 import Itr1.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,12 +16,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import Itr1.Book;
-import Itr1.booksOrganization;
-
 public class IntegrationTest{
     private static booksOrganization library;
     private static Connection connection;
+    private static BookSearch BookSea;
 
     @BeforeAll
     public static void setUp() throws SQLException {
@@ -31,8 +32,79 @@ public class IntegrationTest{
         statement.execute("TRUNCATE TABLE Book");
 
         library = new booksOrganization();
+        BookSea = new BookSearch();
     }
-
+    
+    @Test
+    public void testSearchBookByIdInThreeBooks() throws SQLException {
+BookSea.addBooks("Chinese History", "001", "Tony", "History", 9.99);
+BookSea.addBooks("GuangZhou History", "002", "Tony", "History", 10.99);
+BookSea.addBooks("BeiJing History", "003", "Tony", "History", 8.99);
+        Book result = BookSea.searchByID("002");
+       
+        
+        assertNotNull(result);
+        assertEquals("GuangZhou History", result.getBookName());
+        assertEquals("002",result.getBookId());
+        assertEquals("Tony", result.getAuthor());
+        assertEquals("History", result.getCategory());
+        assertEquals(10.99, result.getPrice(), 0.001);
+       library.removeAllbooks();
+    }
+    
+    @Test
+    public void testSearchBookByNameInThreeBooks() throws SQLException {
+BookSea.addBooks("Chinese History", "001", "Tony", "History", 9.99);
+BookSea.addBooks("GuangZhou History", "002", "Tony", "History", 10.99);
+BookSea.addBooks("BeiJing History", "003", "Tony", "History", 8.99);
+        Book result = BookSea.searchByName("BeiJing History");
+       
+        
+        assertNotNull(result);
+        assertEquals("BeiJing History", result.getBookName());
+        assertEquals("003",result.getBookId());
+        assertEquals("Tony", result.getAuthor());
+        assertEquals("History", result.getCategory());
+        assertEquals(8.99, result.getPrice(), 0.001);
+        library.removeAllbooks();
+    }
+    
+    @Test
+    public void testSearchBookByAuthorInThreeBooks() throws SQLException {
+BookSea.addBooks("Chinese History", "001", "Tony", "History", 9.99);
+BookSea.addBooks("GuangZhou History", "002", "Jack", "History", 10.99);
+BookSea.addBooks("BeiJing History", "003", "Lux", "History", 8.99);
+        Book result = BookSea.searchByAuthor("Jack");
+       
+        
+        assertNotNull(result);
+        assertEquals("Jack", result.getBookName());
+        assertEquals("002",result.getAuthor());
+        assertEquals("GuangZhou History", result.getBookId());
+        assertEquals("History", result.getCategory());
+        assertEquals(10.99, result.getPrice(), 0.001);
+        library.removeAllbooks();
+    }
+    
+    @Test
+    public void testSearchBookByCategoryInThreeBooks() throws SQLException {
+BookSea.addBooks("Chinese History", "001", "Tony", "History", 9.99);
+BookSea.addBooks("GuangZhou History", "002", "Tony", "Arts", 10.99);
+BookSea.addBooks("BeiJing History", "003", "Tony", "Music", 8.99);
+        Book result = BookSea.searchByCategory("Arts");
+       
+        
+        assertNotNull(result);
+        assertEquals("GuangZhou History", result.getBookId());
+        assertEquals("002",result.getAuthor());
+        assertEquals("Tony", result.getBookName());
+        assertEquals("Arts", result.getCategory());
+        assertEquals(10.99, result.getPrice(), 0.001);
+        library.removeAllbooks();
+    }
+    
+    
+    
     @Test
     public void testAddBook() throws SQLException {
         String bookName = "Chinese History";
@@ -54,7 +126,6 @@ public class IntegrationTest{
         assertEquals(price, book.getPrice());
         library.removeAllbooks();
     }
-
     
     @Test
     public void testAddThreeBook() throws SQLException {
@@ -75,7 +146,7 @@ public class IntegrationTest{
         assertEquals(9.99, book.getPrice());
         library.removeAllbooks();
     }
-    
+
     @Test
     public void testDeleteBook() throws SQLException {
         String bookName = "Chinese History";
@@ -88,6 +159,7 @@ public class IntegrationTest{
         assertEquals(0, books.size());
         library.removeAllbooks();
     }
+
     
     @Test
     public void testDeleteSecondBookInThreeBooks() throws SQLException {
@@ -109,7 +181,7 @@ public class IntegrationTest{
         assertEquals(10.99, book.getPrice());
         library.removeAllbooks();
     }
-
+    
     @Test
     public void testRemoveAllBooks() throws SQLException {
         library.addBooks("GuangZhou History", "001", "Tony", "History", 9.99);
