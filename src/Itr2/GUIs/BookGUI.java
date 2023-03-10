e70ace09eee477e6e900df3773ff2306c7ff53a2
AmirTJ <77169817+AmirTJ@users.noreply.github.com>
2023-03-09 19:23:03 -0500

    Update BookGUI.java

--- src/Itr2/GUIs/BookGUI.java
+++ src/Itr2/GUIs/BookGUI.java
@@ -1,121 +1,91 @@
 package Itr2;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.sql.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-public class BookGUI extends JFrame {
-    private Connection conn;
+import java.time.LocalDate;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BorrowTest {
+
     private Book book;
-    private JTextField bookIdField;
-    private JTextField bookNameField;
-    private JTextField authorField;
-    private JTextField categoryField;
-    private JTextField priceField;
-    private JButton saveButton;
-    private JButton clearButton;
+    private User user;
+    private LocalDate borrowDate;
+    private LocalDate returnDate;
+    private Borrow2 borrow;
 
-    public BookGUI() {
-        super("Book Manager");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(6, 2));
-
-        JLabel bookIdLabel = new JLabel("Book ID:");
-        panel.add(bookIdLabel);
-        bookIdField = new JTextField(20);
-        panel.add(bookIdField);
-
-        JLabel bookNameLabel = new JLabel("Book Name:");
-        panel.add(bookNameLabel);
-        bookNameField = new JTextField(20);
-        panel.add(bookNameField);
-
-        JLabel authorLabel = new JLabel("Author:");
-        panel.add(authorLabel);
-        authorField = new JTextField(20);
-        panel.add(authorField);
-
-        JLabel categoryLabel = new JLabel("Category:");
-        panel.add(categoryLabel);
-        categoryField = new JTextField(20);
-        panel.add(categoryField);
-
-        JLabel priceLabel = new JLabel("Price:");
-        panel.add(priceLabel);
-        priceField = new JTextField(20);
-        panel.add(priceField);
-
-        saveButton = new JButton("Save");
-        saveButton.addActionListener(new SaveButtonListener());
-        panel.add(saveButton);
-
-        clearButton = new JButton("Clear");
-        clearButton.addActionListener(new ClearButtonListener());
-        panel.add(clearButton);
-
-        getContentPane().add(panel);
-
-        try {
-            String url = "jdbc:mysql://localhost:3306/library";
-            String user = "root";
-            String password = "123456";
-            conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to database.");
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+    @Before
+    public void setup() {
+        book = new Book("1234", "The Great Gatsby", "F. Scott Fitzgerald", "Fiction", 9.99);
+        user = new User("John Smith", "jsmith123", "password");
+        borrowDate = LocalDate.now();
+        borrow = new Borrow2(book, user, borrowDate);
     }
 
-    private void clearFields() {
-        bookIdField.setText("");
-        bookNameField.setText("");
-        authorField.setText("");
-        categoryField.setText("");
-        priceField.setText("");
+    @Test
+    public void testGetBook() {
+        assertEquals(book, borrow.getBook());
     }
 
-    private class SaveButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            String bookId = bookIdField.getText();
-            String bookName = bookNameField.getText();
-            String author = authorField.getText();
-            String category = categoryField.getText();
-            double price = Double.parseDouble(priceField.getText());
-
-            book = new Book(bookId, bookName, author, category, price);
-
-            String sql = "INSERT INTO Book (bookID, bookName, author, category, price, borrowTime) VALUES (?, ?, ?, ?, ?, ?)";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-
-                stmt.setString(1, book.getBookId());
-                stmt.setString(2, book.getBookName());
-                stmt.setString(3, book.getAuthor());
-                stmt.setString(4, book.getCategory());
-                stmt.setDouble(5, book.getPrice());
-                stmt.setTimestamp(6, new Timestamp(System.currentTimeMillis()));
-                stmt.executeUpdate();
-                JOptionPane.showMessageDialog(null, "Book saved successfully!");
-                clearFields();
-
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-                JOptionPane.showMessageDialog(null, "Error saving book.");
-            }
-        }
+    @Test
+    public void testGetUser() {
+        assertEquals(user, borrow.getUser());
     }
 
-    private class ClearButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            clearFields();
-        }
+    @Test
+    public void testGetBorrowDate() {
+        assertEquals(borrowDate, borrow.getBorrowDate());
     }
 
-    public static void main(String[] args) {
-        BookGUI gui = new BookGUI();
-        gui.setVisible(true);
+    @Test
+    public void testIsBookLent() {
+        assertFalse(borrow.isBookLent());
+        borrow.lendBook();
+        assertTrue(borrow.isBookLent());
     }
+
+    @Test
+    public void testLendBook() {
+        assertFalse(borrow.isBookLent());
+        borrow.lendBook();
+        assertTrue(borrow.isBookLent());
+    }
+
+    @Test
+    public void testReturnBook() {
+        borrow.lendBook();
+        LocalDate returnDate = LocalDate.now().plusDays(14);
+        borrow.returnBook(returnDate);
+        assertFalse(borrow.isBookLent());
+        assertEquals(returnDate, borrow.getReturnDate());
+    }
+
+  
+
+    @Test
+    public void testGetReturnDateBeforeBookReturned() {
+        assertNull(borrow.getReturnDate());
+    }
+
+    @Test
+    public void testReturnBookWithoutLending() {
+        LocalDate returnDate = LocalDate.now().plusDays(14);
+        borrow.returnBook(returnDate);
+        assertFalse(borrow.isBookLent());
+        assertEquals(returnDate, borrow.getReturnDate());
+    }
+
+
+    @Test
+    public void testLendBookMultipleTimes() {
+        borrow.lendBook();
+        borrow.lendBook();
+        borrow.lendBook();
+        assertTrue(borrow.isBookLent());
+    }
+
 }
